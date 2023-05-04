8b5fb69779cb7147785d7c92a851e0d36ab247a0
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-05-04 09:23:39 +0100

    sanity designing

---
+++ components/StudioNavbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { ArrowUturnLeftIcon } from "heroicons/react/24/solid";
+
+
+
+const StudioNavbar = () => {
+  return <div>StudioNavbar
+    <ArrowUturnLeftIcon/>
+  </div>;
+};
+
+export default StudioNavbar;
