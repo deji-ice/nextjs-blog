56d9e008854321946967bd21b7acb99e020167c6
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-05-10 12:31:44 +0100

    TITLE CHANGE

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Blog</title>
+      <title>The curiosity chronicles </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Expand your mind and broaden your horizons with our captivating chronicles of curiosity" />
       <link rel="icon" href="/LOGO.png" />
