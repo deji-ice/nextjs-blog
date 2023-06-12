ca92185c645daa0b3018fe018f451f077dcf5c15
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-13 00:42:18 +0100

    seo optimization

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,15 @@
 export default function Head() {
   return (
     <>
-      <title>The curiosity chronicles</title>
+      <title>The  Curiosity Chronicles</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="author" content="Ayodeji Atanda" />
+      <meta name="keywords" content="tech blog, technology, programming, software development, web development, frontend development, Next.js, javascript, React, TypeScript" />
+
+      <meta name="robots" content="index, follow" />
+      <meta property="og:image" content="/LOGO.png" />
+      <meta property="og:title" content="The Curiosity Chronicles" />
+
       <meta name="description" content="Expand your mind and broaden your horizons with our captivating chronicles of curiosity" />
       <link rel="icon" href="/LOGO.png" />
     </>
