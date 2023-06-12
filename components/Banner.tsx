cb6714a587b9c0a8826872518f4cbf13157475cf
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-12 18:16:16 +0100

    small screen changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Banner = () => {
-  return <div className="flex flex-col lg:flex-row lg:space-x-5 lg:justify-between font-bold px-10 py-5 mb-10 ">
+  return <div className="flex flex-col lg:flex-row lg:space-x-5 lg:justify-between font-bold px-5 py-5 mb-10 ">
     <div>
-<h1 className="text-5xl lg:7xl">The Curiosity Chronicles</h1>
+<h1 className="text-4xl lg:7xl">The Curiosity Chronicles</h1>
 <h2 className="mt-5 lg:mt-2">
   welcome to <span className="underline underline-offset-2 decoration-4 decoration-blue-500">Every developers</span> {" "} favorite blog
 </h2>
--- components/RichTextComponent.tsx
+++ components/RichTextComponent.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+
 export const RichTextComponent = {
   types: {
     image: ({ value }: any) => {
       return (
-        <div className="relative w-full h-96 m-10 mx-auto">
+        <div className="relative w-full h-96 my-5 justify-center">
           <Image
             src={urlFor(value).url()}
             alt="Post Image"
@@ -20,28 +21,32 @@ export const RichTextComponent = {
   },
   list: {
     bullet: ({ children }: any) => (
-      <ul className="ml-3 md:ml-10 py-5 list-disc space-y-5">{children}</ul>
+      <ul className="pl-10 text-lg md:ml-10 py-5 list-disc space-y-5">{children}</ul>
     ),
     number: ({ children }: any) => (
-      <ol className="mt-lg list-decimal">{children}</ol>
+      <ol className="mt-lg text-lg list-decimal">{children}</ol>
     ),
   },
   block: {
     h1: ({ children }: any) => (
-      <h1 className="text-5xl py-10 font-bold">{children}</h1>
+      <h1 className="text-8xl py-3 ">{children}</h1>
     ),
     h2: ({ children }: any) => (
-      <h2 className="text-4xl py-10 font-bold">{children}</h2>
+      <h2 className="text-5xl py-2 font-bold leading-normal">{children}</h2>
     ),
     h3: ({ children }: any) => (
-      <h3 className="text-3xl py-10 font-bold">{children}</h3>
+      <h3 className="text-4xl py-2 text-start font-bold">{children}</h3>
     ),
     h4: ({ children }: any) => (
-      <h4 className="text-2xl py-10 font-bold">{children}</h4>
+      <h4 className="text-4xl py-2 font-bold">{children}</h4>
+    ),
+    normal: ({ children }: any) => (
+      <p className="text-lg py-3 ">{children}</p>
     ),
     blockquote: ({ children }: any) => (
-        <blockquote className="border-l-[blue] border-l-4 pl-5 my-5 bg-zinc-100 py-5">{children}</blockquote>
+        <blockquote className="border-l-[blue]  italic font-medium border-l-4 pl-5 my-5 bg-zinc-100 py-5">{children}</blockquote>
       ),
+    
       
   },
   marks: {
