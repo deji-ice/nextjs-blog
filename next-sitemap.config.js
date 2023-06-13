b57e313dafd606f9cf4e50785d7e143aa7e24f48
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-06-13 02:14:47 +0100

    k

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://curiositychronicles.vercel.app/', // Replace with your website URL
+    generateRobotsTxt: true, // Optional: Generate a robots.txt file
+    exclude: ['/admin'], // Optional: Exclude specific paths from the sitemap
+    sitemapSize: 7000,
+    changefreq: 'daily',
+    priority: 0.7,// Other configuration options...
+  };
+  
--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   images:{
     domains:["links.papareact.com", "cdn.sanity.io"]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
