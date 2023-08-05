92384113acdb11f3f4c3cb78c0573124dd2dffc6
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-05 20:37:08 +0100

    created navigation bar?

--- components/Pathname.tsx
+++ components/Pathname.tsx
@@ -1,15 +1,27 @@
-"use client"
+"use client";
 
 import React from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import { HomeIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { formatPathname } from "@/util/formatPathname";
 
 type Props = {};
 
 const Pathname = (props: Props) => {
-    const pathname = usePathname()
-    return <p className="flex font-medium space-x-1"><Link href={"/"}><HomeIcon className="w-5"/></Link><Link href={`${pathname}`}>{pathname}</Link></p>
+  const pathname = usePathname();
+  const newPathname = pathname ? formatPathname(pathname) : ""
+  return (
+    <p className="flex font-medium space-x-2 md:space-x-1">
+      <Link href={"/"}>
+        <HomeIcon className="w-5 text-gray-500" />
+      </Link>
+      <span>/</span>
+      <Link className="" href={`${pathname}`}>
+        {newPathname}
+      </Link>
+    </p>
+  );
 };
 
 export default Pathname;
---
+++ util/formatPathname.tsx
@@ -0,0 +1,14 @@
+export const formatPathname = (pathname: string): string => {
+    const removedPostPathname = pathname.slice(6); // Remove "/post/" (assuming it's always "/post/")
+    const formattedPathname = removedPostPathname.replace(/-/g, ' '); // Replace hyphens with spaces
+  
+    // Function to capitalize each word
+    const capitalizeEachWord = (str: string): string => {
+      return str.replace(/\b\w/g, (match) => match.toUpperCase());
+    };
+  
+    const capitalizedPathname = capitalizeEachWord(formattedPathname);
+  
+    return capitalizedPathname;
+  };
+  
