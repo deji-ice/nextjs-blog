8860eb376acf9e1b9d66e8b035f9417661f70967
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-09 01:40:45 +0100

    changed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/LOGO.png";
-
+import { MagnifyingGlassIcon, ShareIcon } from "@heroicons/react/24/solid";
 const Header = () => {
   return (
-    <header className="flex justify-between space-x-16 xl:w-screen xl:px-20 overflow-x-clip items-center mt-8 xl:mt-0 mb-5 font-bold px-5 md:px-10  py-5">
-      <div className="flex flex-col items-start ">
-        <Link href={"/"}>
-          <h1 className="hover:cursor-pointer text-2xl leading-6 md:text-4xl text-slate-800 lg:text-5xl">
-            <span className="text-lg md:text-2xl lg:text-4xl">The</span>
-            <br /> Code <br /> Chronicles
-          </h1>
-        </Link>
-        <h2 className="mt-2 text-xs lg:mt-2 lg:text-lg">
+    <header
+      className="flex justify-center bg-slate-950 space-x-16 xl:w-screen xl:px-20 overflow-x-clip items-center
+     mt-8 xl:mt-0 mb-5 font-bold px-5 md:px-10 py-3"
+    >
+      <div className="flex flex-5 gap-5">
+        <MagnifyingGlassIcon className="w-5 text-white hover:cursor-pointer" />
+        <ShareIcon className="w-5 text-white hover:cursor-pointer" />
+      </div>
+
+      <Link className="flex flex-1 flex-col justify-center items-center" href={"/"}>
+        <h1 className="hover:cursor-pointer flex-3 text-2xl leading-6 md:text-4xl text-white lg:text-4xl">
+          <span className="text-xl md:text-2xl lg:text-2xl">The </span>
+          Code Chronicles
+        </h1>
+        <h2 className="mt-2 text-xs lg:mt-2 text-white lg:text-base font-medium">
+          welcome to{" "}
+          <span className="underline underline-offset-2 decoration-2 decoration-white">
+            Every developers
+          </span>{" "}
+          favorite blog
+        </h2>
+      </Link>
+
+      {/* <h2 className="mt-2 text-xs lg:mt-2 lg:text-lg">
           welcome to{" "}
           <span className="underline underline-offset-2 decoration-4 decoration-blue-500">
             Every developers
           </span>{" "}
           favorite blog
-        </h2>
-      </div>
-      <div className="flex flex-col items-end justify-center  ">
-        <Link
-          href={"/"}
-          className="relative px-3 py-2 lg:px-7 lg:py-5 font-bold text-sm lg:text-lg text-white group"
-        >
-          <span
-            className="absolute inset-0 w-full h-full transition duration-300 ease-out transform
-           -translate-x-2 -translate-y-2 bg-slate-900 group-hover:translate-x-0 group-hover:translate-y-0"
-          ></span>
-          <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
-          <span className="relative">Newsletter 🚀 </span>
-        </Link>
-        <p className="mt-5 lg:mt-5 text-gray-400 text-xs hyphens-auto lg:text-lg max-w-[15rem] lg:text-right lg:max-w-[21rem]">
-          New products | Coding solutions
-        </p>
+        </h2> */}
+
+      <div className="flex-col flex-4 justify-center items-center ">
+        
+      <button  className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-slate-700 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
+<span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-slate-800 group-hover:h-full"></span>
+<span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+<svg className="w-5 h-5 text-slate-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+<svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Newsletter</span>
+</button>
       </div>
     </header>
   );
